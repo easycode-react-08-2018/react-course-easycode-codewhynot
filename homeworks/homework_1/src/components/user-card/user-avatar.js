2ba7e1ebb53edb6bb22c9082555e69717b63b52c
codewhynot <codewhynot@gmail.com>
2018-08-29 23:54:01 +0300

    delete

--- homeworks/homework_1/src/components/user-card/user-avatar.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const UserAvatar = props => {
-    const {data} = props;
-    return <img src={data.avatarUrl} className="pull-left"/>
-}
--- homeworks/homework_1/src/components/user-card/user-title.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const UserTitle = props => {
-    const {data} = props;
-    return <h3 className="panel-title">{data.fullName}</h3>
-}
