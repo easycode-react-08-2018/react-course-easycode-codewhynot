6cffbe55aba40557613549a5a960254c87672f19
codewhynot <codewhynot@gmail.com>
2018-09-06 08:36:21 +0300

    homework_3 added

---
+++ homeworks/homework_3/src/components/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+export class App extends Component {
+
+    render () {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
